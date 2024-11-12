e70a4bf60c30364c020bfeb6019983727654525a
Manuel Benavente <mbenaventet@nmgconsulting.biz>
2024-11-12 13:30:52 -0500

    After refactoring the server.js

--- server.js
+++ server.js
@@ -1,58 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { getRedditOAuthToken, getTopPosts, getTopUsers } = require('./redditLogic');
-const redditAPI = require('./redditAPI'); // Import the API calls
-const WebSocket = require('ws');
+const redditAPI = require('./redditAPI'); 
+const { initializeWebSocketServer } = require('./webSocket'); // Import WebSocket logic
 
 const app = express();
-
-// Use the reddit API for all related endpoints
-app.use(redditAPI);
-
-// Enable CORS for all origins
-app.use(cors({
-    origin: 'http://localhost:3000', // React app is running on this port
-    methods: 'GET',
-}));
-
 const PORT = process.env.PORT || 6000;
-const wss = new WebSocket.Server({ port: 6001 });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
+// Middleware
+app.use(cors({ origin: 'http://localhost:3000', methods: 'GET' }));
+app.use(redditAPI); // Reddit API routes
 
-    // Periodically fetch and send top posts and top users to clients
-    const fetchDataAndSend = async () => {
-        try {        
-            const accessToken = await getRedditOAuthToken();
-            const topPosts = await getTopPosts(accessToken);
-            const topUsers = await getTopUsers(accessToken);
-
-            // Ensure that the data is an object with the expected structure
-            const data = { topPosts: topPosts || [], topUsers: topUsers || [] };
-            
-            // Log the data to confirm its structure
-            console.log("Sending data:", data);
-
-            ws.send(JSON.stringify(data));
-        } catch (error) {
-            console.error("Error updating top data:", error);
-        }
-    };
-
-    // Fetch and send data immediately after the client connects
-    fetchDataAndSend();
-
-    // Example interval to fetch and send data every 10 seconds
-    const intervalId = setInterval(fetchDataAndSend, 10000);
-
-    ws.on('close', () => {
-    console.log('Client disconnected');
-    clearInterval(intervalId);
-    });
-});
-
+// Start Express Server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// Initialize WebSocket Server
+initializeWebSocketServer(); 
